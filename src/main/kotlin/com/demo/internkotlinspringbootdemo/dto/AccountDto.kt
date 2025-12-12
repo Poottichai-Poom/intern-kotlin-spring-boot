ec445905c42977c33db13f69b48676e3232a0949
Poottichai-Poom <poottichai05012549@gmail.com>
2025-12-12 09:34:56 +0700

    add1

--- src/main/kotlin/com/demo/internkotlinspringbootdemo/dto/AccountDto.kt
+++ src/main/kotlin/com/demo/internkotlinspringbootdemo/dto/AccountDto.kt
@@ -1,1 +1,31 @@
 package com.demo.internkotlinspringbootdemo.dto
+
+import com.demo.internkotlinspringbootdemo.constants.GenderConstants
+import jakarta.validation.constraints.*
+import java.util.UUID
+
+data class AccountDto(
+
+    val id: UUID? = null,
+
+    @field:NotBlank(message = "firstName must not be blank")
+    val firstName: String,
+
+    @field:NotBlank(message = "lastName must not be blank")
+    val lastName: String,
+
+    @field:NotNull(message = "gender is required")
+    val gender: GenderConstants,
+
+    @field:Pattern(
+        regexp = "^[0-9]10}$",
+        message = "phoneNumber must be 9–10 digits"
+    )
+    val phoneNumber: String? = null,
+
+    @field:Email(message = "email format is invalid")
+    val email: String? = null,
+
+    @field:NotBlank(message = "birthDate must not be blank")
+    val userName: String? = null
+)
--- src/main/kotlin/com/demo/internkotlinspringbootdemo/dto/PetDto.kt
+++ src/main/kotlin/com/demo/internkotlinspringbootdemo/dto/PetDto.kt
@@ -1,1 +1,19 @@
 package com.demo.internkotlinspringbootdemo.dto
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import java.util.UUID
+
+data class PetDto(
+    val id: UUID,
+    val ownerId: UUID,
+
+    @field:NotBlank(message = "name must not be blank")
+    val name: String,
+
+    @field:NotNull(message = "gender is required")
+    val gender: String,
+
+    @field:NotNull(message = "type must not be blank")
+    val type: String,
+)
